2e763def0fa8214078fbb9a1dffa9163c8725b18
Francisco Castillo <castillocerna@gmail.com>
2023-08-21 14:13:15 -0600

    Model OBJ fixed

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from gl import Renderer
 import shaders
 
-# Modifiable parameters for the render
-modelPath = "models/fish.obj"
-texturePath = "textures/fish.bmp"
 backgroundPath = "backgrounds/PARK.bmp"
+outputPath = "output/scene/park.bmp"
 
 width = 1920
 height = 1080
 
-scale = 1
 
 render = Renderer(width, height)
 render.glDirectionalLightDirection(1.3, 0.5, 1.2)
@@ -17,31 +14,50 @@ render.glBackgroundTexture(backgroundPath)
 render.glClearBackground()
 render.vertexShader = shaders.vertexShader
 
+def loadFish(tx, ty, tz, rx, ry, rz):
+    modelPath = "models/fish.obj"
+    texturePath = "textures/fish.bmp"
+    scale = 0.1
     
-def noShader(outputPath):
     render.fragmentShader = shaders.noShader
 
     render.glLoadModel(filename = modelPath,
                      textureName = texturePath,
-                     translate = (0, -1, -5),
+                     translate = (tx, ty, tz),
+                     rotate = (rx, ry, rz),
+                     scale = (scale, scale, scale))
+    
+def loadDog():
+    modelPath = "models/dog.obj"
+    texturePath = "textures/dog.bmp"
+    scale = 0.2
+    
+    render.fragmentShader = shaders.noShader
+
+    render.glLoadModel(filename = modelPath,
+                     textureName = texturePath,
+                     translate = (-2, -2.4, -5),
                      rotate = (0, 45, 0),
                      scale = (scale, scale, scale))
-
-    render.glRender()
-    render.glFinish(outputPath)
     
-# gouradShader("output/shaders/gourad.bmp")
-# thermalToonShader("output/shaders/thermalToon.bmp")
-# negativeThermalShader("output/shaders/negativeThermal.bmp")
-# xRay("output/shaders/xRay.bmp")
-# textureLoader("output/shaders/textureLoader.bmp")
-# textureRemover("output/shaders/textureRemover.bmp")
-# transparent("output/shaders/transparent.bmp")
-# blackAndWhite("output/shaders/blackAndWhite.bmp")
-# contrast("output/shaders/contrast.bmp")
-# adjustedContrast("output/shaders/adjustedContrast.bmp")
-# colorTinting("output/shaders/colorTinting.bmp")
-# posterization("output/shaders/posterization.bmp")
-# gradient("output/shaders/gradient.bmp")
-# nightVision("output/shaders/nightVision.bmp")
-noShader("output/shaders/noShader.bmp")
+def loadSquirrel():
+    modelPath = "models/guy.obj"
+    texturePath = "textures/guy.bmp"
+    scale = 0.05
+    
+    render.fragmentShader = shaders.noShader
+
+    render.glLoadModel(filename = modelPath,
+                     textureName = texturePath,
+                     translate = (2, -2.4, -5),
+                     rotate = (0, -45, 0),
+                     scale = (scale, scale, scale))
+
+# loadFish(0.9, -1, -5, 35, 30, -25)
+# loadFish(1.3, -1.3, -5, 35, 30, -25)
+# loadFish(1.43, -0.9, -5, 35, 30, -25)
+# loadDog()
+loadSquirrel()
+
+render.glRender()
+render.glFinish(outputPath)
